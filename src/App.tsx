e56050ddceb03358b725e76431e2499e028d912e
gracebir <grace.devolop@gmail.com>
2023-03-06 20:29:52 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className='bg-bgLightGray min-h-screen'>
+      <Navbar/>
     </div>
   );
 }
