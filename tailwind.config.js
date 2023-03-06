bf7daca3cba40c57864d0cfc6ce467a11a1d0da9
gracebir <grace.devolop@gmail.com>
2023-03-06 20:27:46 +0200

    config custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        'bgBright':"var(--color-bright-red)",
+        'bgDarkBlue': "var(--color-very-dark-blue)",
+        'bgLightGray': "var(--color-light-gray)",
+        'bgPalered': 'var(--color-very-pale-red)'
+      },
+      colors:{
+        'clGrayish': 'var(--color-dark-grayish)',
+        'clDarkBlue': 'var(--color-dark-blue)',
+        'clVdarkBlue': 'var(--color-very-dark-blue)',
+        'clLightGray': 'var(--color-light-gray)'
+      }
+    },
   },
   plugins: [],
 }
