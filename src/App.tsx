c4d6eb2e0f59bb2da1ae7ee97d26b2967c9a356a
gracebir <grace.devolop@gmail.com>
2023-03-07 15:07:34 +0200

    hamburger navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='bg-bgLightGray min-h-screen'>
+    <div className='bg-bgLightGray min-h-screen font-vietnam'>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
