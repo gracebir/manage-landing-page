e5917c2210091438c6401136a70ef382cbf2815f
gracebir <grace.devolop@gmail.com>
2023-03-08 16:18:43 +0200

    about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import About from './components/About/About';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='bg-bgLightGray min-h-screen font-vietnam'>
+    <div className='bg-bgLightGray min-h-screen lg:bg-bgHero lg:bg-no-repeat bg-contain lg:bg-right-top'>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   );
 }
