440cbb43b4f5c49807d2cc7c6cfa1f0958244194
gracebir <grace.devolop@gmail.com>
2023-03-17 21:34:21 +0200

    add the two component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import About from './components/About/About'
 
 function App() {
   return (
     <div className="bg-bgLightGray min-h-screen lg:bg-bgHero lg:bg-no-repeat bg-contain lg:bg-right-top">
       <Navbar/>
+      <Hero/>
+      <About/>
     </div>
   )
 }
