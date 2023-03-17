78cddf21422993774cf840919136297a1c1942a9
gracebir <grace.devolop@gmail.com>
2023-03-17 21:28:13 +0200

    navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="bg-black text-white flex justify-center items-center h-screen">
-      <h1 className='font-bold text-7xl'>hello world</h1>
+    <div className="bg-bgLightGray min-h-screen lg:bg-bgHero lg:bg-no-repeat bg-contain lg:bg-right-top">
+      <Navbar/>
     </div>
   )
 }
