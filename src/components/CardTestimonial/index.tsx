cc19b2101aa344e40e46e57290e5567edcfb696e
gracebir <grace.devolop@gmail.com>
2023-06-23 19:12:42 +0200

    getstarted components

--- src/components/CardTestimonial/index.tsx
+++ src/components/CardTestimonial/index.tsx
@@ -3,7 +3,7 @@ import { Ttestimonial } from '../type'
 
 function Card({ desc, fullname, img }: Ttestimonial) {
     return (
-        <div className='inline-flex flex-col lg:gap-4 gap-3 items-center whitespace-normal w-[600px] h-[30vh] m-2'>
+        <div className='inline-flex flex-col lg:gap-4 gap-3 items-center whitespace-normal lg:w-[600px] w-full h-[30vh] m-2'>
             <img className='h-16 w-16 -mb-12 z-20' src={img} alt={fullname} />
             <div className='bg-gray-100 w-full px-4 py-12 flex flex-col gap-6 h-full'>
                 <h3 className='text-center text-lg font-semibold text-clVdarkBlue'>{fullname}</h3>
--- src/components/Getstart/index.tsx
+++ src/components/Getstart/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
 function GetStart() {
   return (
-    <div>
-      
+    <div className='w-full lg:bg-simple-desk lg:bg-right-top bg-left bg-bgBright bg-simple-mob bg-no-repeat py-16'>
+      <div className='w-full h-full'>
+        <div className='container mx-auto flex lg:justify-between lg:flex-row flex-col gap-8 lg:gap-0 items-center text-white'>
+          <h1 className='lg:text-3xl text-5xl text-center lg:text-start leading-[1.2] font-medium lg:font-bold'>Simplify how your team <br className='hidden lg:block' /> works today.</h1>
+          <button className='bg-white text-red-color px-6 py-3 rounded-full'>Get Started</button>
+        </div>
+      </div>
     </div>
   )
 }
